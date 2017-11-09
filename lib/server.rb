625ee92d12c01d442d7e1edbb74a7a5dc9deeee4
Hang Li Li <hang1997li@gmail.com>
2017-11-09 23:11:31 +0000

    Implemented simple variable prices by vehicle

--- lib/server.rb
+++ lib/server.rb
@@ -5,14 +5,29 @@ module GameOfShutl
   class Server < Sinatra::Base
     post '/quotes' do
       quote = JSON.parse(params['quote'])
-
+      vehicleType = quote['vehicle']
       price = ((quote['pickup_postcode'].to_i(36) - quote['delivery_postcode'].to_i(36)) / 1000).abs
-
+      if (vehicleType == "bicycle")
+        price = price + price * 0.1
+      elseif (vehicleType == "motorbike")
+        price = price + price * 0.15
+      elseif (vehicleType == "parcel_car")
+        price = price + price * 0.2
+      elseif (vehicleType == "small_van")
+        price = price + price * 0.3
+      elseif (vehicleType == "large_van")
+        price = price + price * 0.4
+      else
+        price = 0
+        puts "Invalid vehicle"
+        vehicleType = "Invalid vehicle"
+      end
       {
         quote: {
           pickup_postcode: quote['pickup_postcode'],
           delivery_postcode: quote['delivery_postcode'],
-          price: price
+          price: price,
+          vehicle: vehicleType
         }
       }.to_json
     end
